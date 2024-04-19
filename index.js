25113f94bf9fed509982fc274cf9903dde72f7fc
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-04-19 09:32:14 +0600

    clien side post data

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
 const port = process.env.PORT || 4000;
+
+app.use(cors());
 
 const user = [
       {id: 1, name: "John", email: "john@example.com"},
@@ -12,6 +14,13 @@ const user = [
 app.get('/users', (req, res) => {
       res.send(user);
 })
+
+app.post('/users', (req, res) => {
+      console.log("post api hiting status");
+      console.log(req.body);
+})
+
+
 
 app.get('/', (req, res) => {
       res.send('Welcome to the server')
